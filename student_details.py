33e19ddc40a118e9631c94a3b3a36e741417b545
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:24 +0530

    My First Commit

---
+++ student_details.py
@@ -0,0 +1,10 @@
+
+name=input("Enter the name of the student:")
+roll_no=int(input("Enter the roll number:"))
+cgpa=float(input('Enter your CGPA:'))
+percentage=cgpa*10
+print("Student Name:",name)
+print("Roll No:",roll_no)
+print("Your percentage is:",percentage,"%")
+
+
