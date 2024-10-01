b7066d287ec934830a739d0e56dae044924cce78
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-10-01 15:25:33 +0530

    Document Header Added

--- student_details.py
+++ student_details.py
@@ -1,3 +1,8 @@
+'''Author:Savio Bijo Thomas
+Date:01-10-2-24
+Python Program To Get The Student Details
+Version:1.0'''
+
 
 name=input("Enter the name of the student:")
 roll_no=int(input("Enter the roll number:"))
